caab9f040a3c265d17ffe3b7942bd289fe4f73d0
WangHngLeee <1023836798@qq.com>
2020-02-29 16:50:24 -0500

    add two questions with same solutions

---
+++ BinarySearch/852. Peak Index in a Mountain Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums == null || nums.length == 0)return -1;
+        int left = 0;
+        int right = nums.length -1;
+        while(left + 1< right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] < nums[mid + 1]){
+                left = mid;
+            }else{
+                right = mid;
+            }
+        }
+        if(nums[left]>nums[right])return left;
+        else return right;
+    }
+}
+//每次对mid和mid+1比较。更新左右边界即可。
